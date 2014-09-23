794bb0e2ce83b0f71d9c992ce7e9803f08177084
Nick Jones <jonesnickb@gmail.com>
2014-09-22 19:43:19 -0700

    added functions

--- js/main.js
+++ js/main.js
@@ -46,13 +46,6 @@ var createGrid = function(grid){
 	}
 }
 
-//Testing out interactions
-/*$("#grid rect").click(function(event){
-	console.log("in click event")
-	console.log(this)
-	console.log(event);
-})*/
-
 //initialize the visualization
 $(document).ready(function(){
 	var grid = new Grid; 
@@ -64,8 +57,33 @@ $(document).ready(function(){
 	createGrid(grid);
 
 	//Get first item
-
+	$('#nextItem').html(getItem());
+	
+	
+	$('rect').click(function(event){
+		$('#nextItem').html(getItem());
+		console.log(
+			this.getAttribute('row'),
+			this.getAttribute('column')
+			// event.currentTarget.attributes.row.value, 
+			// event.currentTarget.attributes.column.value
+		);
+	})
 })
 
-
-
+function getItem() {
+	var item = '';
+	var rand = Math.round(Math.random() * 100);
+	
+	if (rand >= 0 && rand <= 70) {
+		item = "grass";
+	} else if (rand > 70 && rand <= 94) {
+		item = "bush";
+	} else if (rand > 94 && rand <= 99) {
+		item = "tree";
+	} else if (rand > 99 && rand <= 100) {
+		item = "hut";
+	}
+	
+	return item;
+}
