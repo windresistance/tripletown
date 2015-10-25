a2a5d7fe7c5e9990fde14dcaab5c4da67c1700c5
Nick Jones <jonesnickb@gmail.com>
2015-10-24 17:25:48 -0700

    recursive fill function is working

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
+var items = ["grass", "bush", "tree", "hut", "house", "bighouse", "castle"];
 
 //Sets up properties for the grid
 var Grid = function(){
-	
 	return {
 		rows: 6,
-		columns: 6,
+		cols: 6,
 		width: 60,
 		height: 60,
 		spacing: 10,
@@ -17,9 +17,9 @@ var Grid = function(){
 	};	
 }
 
-//Scales the SVG based on the number of columns and rows
+//Scales the SVG based on the number of cols and rows
 var setupSVG = function(grid){
-	var w = grid.columns*grid.width + (grid.columns -1)*grid.spacing + 200 + grid.margin.left + grid.margin.right;
+	var w = grid.cols*grid.width + (grid.cols -1)*grid.spacing + 200 + grid.margin.left + grid.margin.right;
 	var h = grid.rows*grid.height + (grid.rows - 1)*grid.spacing + grid.margin.top + grid.margin.bottom;
 
 	$("svg#game").attr("width", w);
@@ -43,7 +43,7 @@ var createGrid = function(grid){
 	addToSVG("game", gridGroup);
 
 	for (var i =0; i < grid.rows; i++){
-		for (var j=0; j < grid.columns; j++){
+		for (var j=0; j < grid.cols; j++){
 
 			//finding the x and y coordinates
 			var x = j*(grid.width + grid.spacing);
@@ -51,7 +51,7 @@ var createGrid = function(grid){
 			
 			var new_rect = createSVGEl("rect");
 
-			var attrs = {"x":x, "y":y , "height": grid.width, "width": grid.width, "class":"empty" , "column": j, "row": i}
+			var attrs = {"x":x, "y":y , "height": grid.width, "width": grid.width, "class":"empty" , "col": j, "row": i}
 
 			setAttributes(new_rect, attrs)
 
@@ -59,6 +59,8 @@ var createGrid = function(grid){
 
 		}
 	}
+
+	addInitialItems(grid);
 }
 
 var getItem = function() {
@@ -97,7 +99,7 @@ function randBetween(beg,end) {
 
 var addInitialItems = function(grid){
 
-	var boxes = grid.rows*grid.columns;
+	var boxes = grid.rows*grid.cols;
 	var lowerBound = Math.floor(boxes*.25);
 	var upperBound = Math.floor(boxes*.5);
 
@@ -106,16 +108,16 @@ var addInitialItems = function(grid){
 	//iterate through item numbers to add
 	//initial items
 	for (var i=0; i < randItems; i++){
-		var x = Math.round(randBetween(0, grid.columns));
+		var x = Math.round(randBetween(0, grid.cols));
 		var y = Math.round(randBetween(0, grid.rows));
 
-		$("svg rect[column=" + x + "][row=" + y +"]").attr("class", getItem());
+		$("svg rect[col=" + x + "][row=" + y +"]").attr("class", getItem());
 	}
 }
 
 var createItemPanel = function(grid){
 
-	var x = grid.columns*grid.width + (grid.columns -1)*grid.spacing + 30;
+	var x = grid.cols*grid.width + (grid.cols -1)*grid.spacing + 30;
 
 	var y = 0;
 
@@ -175,43 +177,105 @@ var setAttributes = function(element, attrs){
 	}
 }
 
+var floodFill = function(node, replace_class, matches, checked) {
+
+	var col = parseInt(node.attr('col'));
+	var row = parseInt(node.attr('row'));
+	var key = row+"-"+col;
+
+	// if node has already been checked, exit
+	if (checked.indexOf(key) >= 0) {
+		return;
+	}
+
+	// push checked node onto checked array
+	checked.push(key);
+
+	// if match, push onto matches array, else stop
+	if (node.attr('class') == replace_class) {
+		matches.push(node);
+	} else {
+		return;
+	}
+
+	floodFill($("svg rect[col=" + col + "][row=" + (row-1) +"]"), replace_class, matches, checked);  // N
+	floodFill($("svg rect[col=" + col + "][row=" + (row+1) +"]"), replace_class, matches, checked);  // S
+	floodFill($("svg rect[col=" + (col-1) + "][row=" + row +"]"), replace_class, matches, checked);  // E
+	floodFill($("svg rect[col=" + (col+1) + "][row=" + row +"]"), replace_class, matches, checked);  // W
+
+	return;
+}
+
+var checkGameOver = function() {
+	return ($("svg rect.empty").length == 0);
+}
+
+var growItem = function(clickedBox, item){
+	console.log('growItem','\nclickedBox:',clickedBox,'\nitem:',item);
+	var newClass = item.attr('class');
+	var matches = [];
+	var checked = [];
+
+	item.attr('class', newClass);  // set clicked box to new item
+
+	floodFill($(item), newClass, matches, checked);
+
+	console.log(matches);
+
+	if (matches.length >= 3) {
+		matches.forEach(function(match){
+			match.attr('class','empty');
+		})
+		var nextClass = items.indexOf(item.attr('class')) + 1;
+		clickedBox.attr('class', items[nextClass]);
+
+		growItem(item, clickedBox, items[nextClass])
+	}
+}
+
 //initialize the visualization
 $(document).ready(function(){
-	var grid = new Grid; 
-	
-	//Setup svg
+	var grid = new Grid;
 	setupSVG(grid);
-	
-	//Call function to setup grid
 	createGrid(grid);
-
-	addInitialItems(grid);
-
 	createItemPanel(grid);
-	
+
 	// on click event
 	$('rect').click(function(event){
-		var col = this.getAttribute('column');
-		var row = this.getAttribute('row');
+		var newItem = $('#newItem');  // get new item
+		var col = parseInt($(this).attr('col'));
+		var row = parseInt($(this).attr('row'));
 		
-		var clickedBox = $("svg rect[column=" + col + "][row=" + row +"]");
+		//var clickedBox = $("svg rect[col=" + col + "][row=" + row +"]");
 		
-		if (clickedBox.attr("class") === "empty") {
-			
-			// place item
-			$('rect').animate({borderWidth:"2px"});
-			
-			clickedBox.attr("class", $('#newItem').attr('class'));
-			
-			// check neighbors...
-			
-			
-			// get next item
-			$('#newItem').attr('class',getItem());
+		if ($(this).attr("class") === "empty") {
+			// check neighbors
+			growItem($(this), newItem);
+			// var checked = [];
+
+			// $(this).attr('class',newItem.attr('class'));  // set clicked box to new item
+
+			// floodFill($(this), newItem.attr('class'), matches, checked);
+
+			// if (matches.length >= 3) {
+			// 	matches.forEach(function(match){
+			// 		match.attr('class','empty');
+			// 	})
+			// 	var newClass = items.indexOf(newItem.attr('class')) + 1;
+			// 	clickedBox.attr('class',items[newClass]);
+			// }
+
+			// place newItem
+			// $('rect').animate({borderWidth:"2px"});
+			// clickedBox.attr("class", newItem.attr('class'));
+
+			// get next newItem
+			newItem.attr('class',getItem());
+
+			if('is game over?',checkGameOver()) {
+				$('#gameOver').text("GAME OVER!");
+			}
 		}
-			
-		
-		
 	})
 })
 
